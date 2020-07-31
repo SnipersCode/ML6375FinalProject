520b3263f4c5e51d51f80946cb28c596826c1d2a
James <validtechrange@gmail.com>
2020-07-31 09:50:55 -0500

    Use TF Agents Environment
    Swap out standard atari openai gym with tf_agents wrapper

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from typing import List, Union
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
-import gym
 import cv2
+
+import tensorflow as tf
+from tf_agents.environments import suite_atari
+
+tf.compat.v1.enable_v2_behavior()
 
 
 class Game:
@@ -15,12 +19,17 @@ class Game:
         self.frame_history: List[np.ndarray] = []
         self.current_frame: np.ndarray = np.array([])
         self.current_reward: float = 0.0
-        self.env_done: bool = False
-        self.env_info: dict = {}
+        self.current_discount: float = 0.0
+        self.step_type = None
 
     def __enter__(self):
-        self.env = gym.make('BreakoutDeterministic-v4')
+        self.env = suite_atari.load('BreakoutDeterministic-v4')
         self.env.reset()
+        print(self.env.time_step_spec().step_type)
+        print(self.env.time_step_spec().reward)
+        print(self.env.time_step_spec().discount)
+        print(self.env.time_step_spec().observation)
+        print(self.env.action_spec())
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -28,16 +37,18 @@ class Game:
 
     @staticmethod
     def _preprocess(img: np.ndarray) -> np.ndarray:
-        shape = img.shape
-        # Reduce inputs by converting from RGB to Grayscale. Colors in game does not affect score.
-        new_img = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_RGB2GRAY)
-        # Resize image to reduce amount of processing needed
-        return cv2.resize(new_img, (shape[1]//2, shape[0]//2))
+        return img
+        # shape = img.shape
+        # # Reduce inputs by converting from RGB to Grayscale. Colors in game does not affect score.
+        # print(img.shape)
+        # new_img = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_RGB2GRAY)
+        # # Resize image to reduce amount of processing needed
+        # return cv2.resize(new_img, (shape[1] // 2, shape[0] // 2))
 
-    def random_action(self) -> object:
-        return self.env.action_space.sample()
+    def random_action(self):
+        return np.array(self.env.action_space.sample())
 
-    def next_state(self, action: Union[object, None] = None, render=False):
+    def next_state(self, action=None, render=False):
 
         # Perform random action if action is None
         if not action:
@@ -46,7 +57,7 @@ class Game:
             actual_action = action
 
         # Tell game to move forward one frame
-        self.current_frame, self.current_reward, self.env_done, self.env_info = self.env.step(actual_action)
+        self.step_type, self.current_reward, self.current_discount, self.current_frame = self.env.step(actual_action)
 
         # Add to frame memory
         if len(self.frame_history) == 0:
@@ -70,8 +81,8 @@ def main():
     with Game() as game:
         print("Running game...")
         for _ in range(200):
-            time.sleep(1/max_fps)
-            game.next_state(render=False)
+            time.sleep(1 / max_fps)
+            game.next_state(render=True)
 
         print("Showing frame history...")
         fig = plt.figure()
